1fd86be9d5fb9e0229ac071fdc612b6caa24be54
santiwg <santiwgwuri@gmail.com>
2025-03-12 13:36:39 -0300

    Add UI components

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log('Hello world')
 
 function getComputerChoice(){
     num=Math.random();
@@ -12,58 +11,16 @@ function getComputerChoice(){
         return 'scissors';
     }
 }
-function getHumanChoice(){
+/*function getHumanChoice(){
     choice=prompt('Enter your choice (Rock, Paper or Scissors)').toLowerCase();
     while (choice!='rock' && choice!='paper' && choice!='scissors' ){
         choice=prompt('Invalid input:Enter Rock, Paper or Scissors').toLowerCase();
     }
     return choice;
-}
+}*/
 
-function playGame(){
-    function playRound(humanChoice,computerChoice){
-        if (humanChoice=='rock'){
-            if (computerChoice=='rock'){
-                console.log('Tied round!')
-            }
-            else if(computerChoice=='paper'){
-                computerScore++;
-                console.log('Computer wins the round D:')
-            }
-            else{
-                humanScore++;
-                console.log('Human wins the round :D')
-            }
-        }
-        else if(humanChoice=='paper'){
-            if (computerChoice=='rock'){
-                humanScore++;
-                console.log('Human wins the round :D')
-            }
-            else if(computerChoice=='paper'){
-                console.log('Tied round!')
-            }
-            else{
-                computerScore++;
-                console.log('Computer wins the round D:')
-            }
-        }
-        else{
-            if (computerChoice=='rock'){
-                computerScore++;
-                console.log('Computer wins the round D:')
-            }
-            else if(computerChoice=='paper'){
-                humanScore++;
-                console.log('Human wins the round :D')
-            }
-            else{
-                console.log('Tied round!')
-            }
-        }
-    }
-    let humanScore=0;
-    let computerScore=0;
+/*function playGame(){
+    
     playRound(getHumanChoice(),getComputerChoice());
     playRound(getHumanChoice(),getComputerChoice());
     playRound(getHumanChoice(),getComputerChoice());
@@ -78,4 +35,56 @@ function playGame(){
     else{
         console.log('Computer wins the game!')
     }
+}*/
+function playRound(humanChoice,computerChoice){
+    if (humanChoice=='rock'){
+        if (computerChoice=='rock'){
+            console.log('Tied round!')
+        }
+        else if(computerChoice=='paper'){
+            computerScore++;
+            console.log('Computer wins the round D:')
+        }
+        else{
+            humanScore++;
+            console.log('Human wins the round :D')
+        }
+    }
+    else if(humanChoice=='paper'){
+        if (computerChoice=='rock'){
+            humanScore++;
+            console.log('Human wins the round :D')
+        }
+        else if(computerChoice=='paper'){
+            console.log('Tied round!')
+        }
+        else{
+            computerScore++;
+            console.log('Computer wins the round D:')
+        }
+    }
+    else{
+        if (computerChoice=='rock'){
+            computerScore++;
+            console.log('Computer wins the round D:')
+        }
+        else if(computerChoice=='paper'){
+            humanScore++;
+            console.log('Human wins the round :D')
+        }
+        else{
+            console.log('Tied round!')
+        }
+    }
 }
+let humanScore=0;
+let computerScore=0;
+const rock=document.querySelector('rock');
+const paper=document.querySelector('paper');
+const scissors=document.querySelector('scissors');
+const humanChoice=document.querySelector('human-choice');
+const computerChoice=document.querySelector('computer-choice');
+const humanPoints=document.querySelector('human-points');
+const computerPoints=document.querySelector('computer-points');
+const winner=document.querySelector('winner');
+const reset=document.querySelector('reset')
